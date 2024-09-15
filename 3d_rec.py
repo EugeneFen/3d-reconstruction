e1aad4153d053163b977ec5bfe4ecf97fac5d366
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2024-09-15 23:12:05 +1000

    Update 3d_rec.py
    Собран полностью код

--- 3d_rec.py
+++ 3d_rec.py
@@ -1,117 +1,116 @@
-import cv2 as cv2
+import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import open3d as o3d
-from scipy.spatial import Delaunay
 
-# read left and right images
-imgR = cv2.imread("img14.jpg", 0)
-imgL = cv2.imread("img15.jpg", 0)
-F = 6
+# Параметры камеры
+focal_length = 6338.47
+baseline = 171.548
 
-# уменьшение качества изображения, бля уменьшения точек изображения
-scaler_factor = 0.9
-new_size = (int(imgL.shape[1]*scaler_factor), int(imgL.shape[0]*scaler_factor))
+# Загрузите стерео изображения
+imgL = cv2.imread('im21.png')
+imgR = cv2.imread('im22.png')
 
-imgL = cv2.resize(imgL, new_size)
-imgR = cv2.resize(imgR, new_size)
+# Преобразуйте изображения в градации серого
+grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 
-# создание карты глубины
-stereo = cv2.StereoBM_create(numDisparities=16,
-                            blockSize=15)
+# Создайте объект StereoSGBM для вычисления карты глубины
+stereo = cv2.StereoBM_create(numDisparities=16, blockSize=11)
+disparity = stereo.compute(grayL, grayR)
+print("Disparity good")
+# print("Dispar min", np.min(disparity))
+# print("Dispar max", np.max(disparity))
 
-disparity = stereo.compute(imgL, imgR)
+# Нормализуйте карту глубины
+disparity = cv2.normalize(disparity, None, 0, 255, cv2.NORM_MINMAX)
+disparity = np.uint8(disparity)
 
-# нормальзация карты глубины
-disparity_normalized = cv2.normalize(disparity, None, 0, 255, cv2.NORM_MINMAX)
+#-----------------------------------------------------------------------------
+# блок создания трехмерных точек
+# Преобразуйте карту глубины в 3D-координаты
+h, w = disparity.shape
+Q = np.float32([[1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 0, -focal_length],
+                [0, 0, 1/baseline, 0]])
 
-# размеры карты глубины
-height, width = disparity_normalized.shape
+points_3D = cv2.reprojectImageTo3D(disparity, Q)
 
-# print(height) #ширина
-# print(width) #высота
+# Маска для действительных значений
+mask = disparity > disparity.min()
 
-# Вывод карты глубины как массива
-# for i in disparity_normalized:
-#     print(i)
+# Извлеките действительные точки и цвета
+valid_points = points_3D[mask]
+valid_colors = cv2.cvtColor(grayL, cv2.COLOR_GRAY2RGB)[mask]
+#----------------------------------------------------------------------------
 
-#Вывод карты глубины
-# plt.imshow(disparity_normalized, cmap='plasma')
-# plt.colorbar()
-# plt.title('DM')
+#-------------------------------------------------------------------
+# блок сохранения облака точек
+# Преобразуйте в формат, подходящий для Open3D
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(valid_points)
+pcd.colors = o3d.utility.Vector3dVector(valid_colors / 255.0)
+
+# Сохраните облако точек в файл
+o3d.io.write_point_cloud('point_cloud.ply', pcd)
+
+print("Облако точек сохранено в 'point_cloud.ply'")
+#-------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------
+# Визуализация 3D-точек
+# valid_points = points_3D
+# x = valid_points[:, :, 0].flatten()
+# y = valid_points[:, :, 1].flatten()
+# z = valid_points[:, :, 2].flatten()
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(x, y, z, c='r', marker='o', s=1)
+#
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+# ax.set_title('3D Point Cloud')
+#
 # plt.show()
 
-# Определение списков точек
-point3d = np.empty((0, 3), float)
-arrCount3d = np.array([])
-arrPoint3d = np.empty((0, 3), float)
+# визуализация по другому
+o3d.visualization.draw_geometries([pcd])
+#-------------------------------------------------------------------------
 
-# фокусное расстояние
-F = 3
-
-#Создание трехреных точек
-for i in range(len(disparity_normalized)): #x
-    for j in range(len(disparity_normalized[1])): #y
-        Z = disparity_normalized[i][j]
-        X = ((i-width/2) * Z)/F
-        Y = ((j-height/2) * Z)/F
-        arrCount3d = np.append(arrCount3d, [round(X, 2), round(Y, 2), round(Z, 2)])
-        arrPoint3d = np.vstack([arrPoint3d, arrCount3d])
-        arrCount3d = np.empty((0, 3), float)
-    point3d = np.vstack([point3d, arrPoint3d])
-    arrPoint3d = np.empty((0, 3), float)
-
-# Занесение координат в точки
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(point3d)
-
-# визуализация точек
-# o3d.visualization.draw_geometries([pcd])
-
+#-------------------------------------------------------------------------------
 # Фильтрация статистических выбросов
 cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
 filtered_pcd = pcd.select_by_index(ind)
+print("Filter good")
+#------------------------------------------------------------------------------
 
-# Визуализация до и после фильтрации
-# o3d.visualization.draw_geometries([pcd], window_name="Original Point Cloud")
-# o3d.visualization.draw_geometries([filtered_pcd], window_name="Filtered Point Cloud")
 
-# Создание триангуляции
-tri = Delaunay(filtered_pcd[:, :2])
+# Извлечение массива точек
+points = np.asarray(pcd.points)
+
+# print(points)
+
+# Создание трингуляции Делоне
+tri = Delaunay(points)
+
+print("Delaunay good")
 
 # Визуализация
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(filtered_pcd[:, 0], filtered_pcd[:, 1], filtered_pcd[:, 2], color='r')
+ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='r')
 
+# Добавление тетраэдров
 for simplex in tri.simplices:
-    simplex = np.append(simplex, simplex[0])
-    ax.plot(filtered_pcd[simplex, 0], filtered_pcd[simplex, 1], filtered_pcd[simplex, 2], 'k-')
+    tetra = [points[simplex[i]] for i in range(4)]
+    poly3d = [[tetra[0], tetra[1], tetra[2]],
+              [tetra[0], tetra[1], tetra[3]],
+              [tetra[1], tetra[2], tetra[3]],
+              [tetra[0], tetra[2], tetra[3]]]
+    ax.add_collection3d(Poly3DCollection(poly3d, alpha=.25, linewidths=1, edgecolors='r'))
 
 plt.show()
-
-
-
-# визуализация отфильтрованных точек другим способом
-# fig = plt.figure(figsize=(4,4))
-# ax = fig.add_subplot(111, projection='3d')
-# x = [point[0] for point in point3d]
-# y = [point[1] for point in point3d]
-# z = [point[2] for point in point3d]
-#
-# # Создаем экземпляр Axes3D
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# print(X)
-#
-# # Визуализируем точки
-# ax.scatter(x, y, z)
-#
-# # Устанавливаем заголовок и подписываем оси
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
-#
-# # Показываем график
-# plt.show()
