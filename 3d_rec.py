2f7dd2014ceff05fe34eeecf2eed5dd4c479447a
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2024-10-19 23:00:34 +1000

    Update 3d_rec.py

--- 3d_rec.py
+++ 3d_rec.py
@@ -1,270 +1,177 @@
-import math
-import random
-
-import cv2
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+from PIL import Image
+import torch
+from transformers import GLPNFeatureExtractor, GLPNForDepthEstimation
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial import Delaunay
-from mpl_toolkits.mplot3d import Axes3D
 import open3d as o3d
-import re
+import cv2
 
-def find_new_point(data, i):
-    # Определение центра вращения
-    x_center = np.mean(data[:, 0])
-    y_center = np.mean(data[:, 1])
-    z_center = np.min(data[:, 2])
-    print(z_center)
-    # for row in data:
-    #     if row[2] == z_center:
-    #         x_center = row[0]
-    #         y_center = row[1]
-    center = np.array([x_center, y_center, z_center])
-    print(data)
-    print("And ")
+feature_extractor = GLPNFeatureExtractor.from_pretrained("vinvino02/glpn-nyu")
+model = GLPNForDepthEstimation.from_pretrained("vinvino02/glpn-nyu")
 
-    # Угол альфа (в радианах)
-    alpha = np.radians(20*i)  # Например, для угла 45 градусов
+def get_map():
+    image = Image.open("imeg/Img1.jpg")
 
-    # Матрица поворота для вращения вокруг оси Z
-    def rotation_matrix_z(theta):
-        # return np.array([[np.cos(theta), -np.sin(theta), 0],
-        #                  [np.sin(theta), np.cos(theta), 0],
-        #                  [0, 0, 1]])
-        return np.array([[np.cos(theta), 0, np.sin(theta)],
-                         [0, 1, 0],
-                         [-np.sin(theta), 0, np.cos(theta)]])
+    new_height = 480 if image.height > 480 else image.height
+    new_height -= (new_height % 32)
+    new_width = int(new_height * image.width / image.height)
+    diff = new_width % 32
+    new_width = new_width - diff if diff < 16 else new_width + 32 - diff
+    new_size = (new_width, new_height)
+    image = image.resize(new_size)
 
-    # Матрица поворота
-    rotation_matrix = rotation_matrix_z(alpha)
+    # prepare image for the model
+    inputs = feature_extractor(images=image, return_tensors="pt")
 
-    # Перенос данных к центру вращения
-    data_centered = data - center
+    # get the prediction from the model
+    with torch.no_grad():
+        outputs = model(**inputs)
+        predicted_depth = outputs.predicted_depth
 
-    # Поворот данных
-    data_rotated = np.dot(data_centered, rotation_matrix.T) + center
-    print(data_rotated)
+    # remove borders
+    pad = 16
+    output = predicted_depth.squeeze().cpu().numpy() * 1000.0
+    output = output[pad:-pad, pad:-pad]
 
-    return data_rotated
+    # Инвертируем значения пикселей
+    output_inverted = np.max(output) - output
 
+    # Нормализуем значения, если нужно
+    output_inverted = (output_inverted - np.min(output_inverted)) / (np.max(output_inverted) - np.min(output_inverted))
 
-# Параметры камеры
-focal_length = 2000.47
-baseline = 110.548
+    image = image.crop((pad, pad, image.width - pad, image.height - pad))
 
+    # visualize the prediction
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(image)
+    ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax[1].imshow(output, cmap='plasma')
+    ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+    plt.tight_layout()
+    plt.pause(5)
+    return output_inverted, image
 
-first = 'imeg/Img'
-end = '.jpg'
-
-img_list1 = ['img30.jpg', 'img31.jpg', 'img32.jpg', 'img33.jpg']
-img_list2 = ['img34.jpg', 'img35.jpg', 'img36.jpg', 'img37.jpg']
-img_list = img_list2
-glodal_cloud_point = np.array([])
-points_global = np.array([])
-corner = 0
-dispart_late = np.uint8()
-
-#проверку размера файла
-
-# Загрузите стерео изображения
-# imgL = cv2.imread('im21.png')
-# imgR = cv2.imread('im22.png')
-
-# for i in range(len(img_list)-1):
-for i in range(1,10):
-    # if re.fullmatch(r"\w*.jpg|.png", img_list[i]):
-    #     print('True')
-    # imgL = cv2.imread(img_list[i])
-    # imgR = cv2.imread(img_list[i+
-
-    print(first + str(i) + end)
-
-    imgL = cv2.imread(first + str(i) + end)
-    imgR = cv2.imread(first + str(i+1) + end)
+def get_disparity():
+    # Загружаем изображения
+    imgL = cv2.imread('imeg/Img1.jpg')
+    imgR = cv2.imread('imeg/Img2.jpg')
 
     # Преобразуйте изображения в градации серого
     grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
     grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 
     # Создайте объект StereoSGBM для вычисления карты глубины
-    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=11)
+    stereo = cv2.StereoBM_create(numDisparities=48, blockSize=15)
     disparity = stereo.compute(grayL, grayR)
-    print("Disparity good")
-    # print("Dispar min", np.min(disparity))
-    # print("Dispar max", np.max(disparity))
+    disparity_normalized = cv2.normalize(disparity, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    # return disparity_normalized
+    # Отображение или сохранение
+    # cv2.imshow('Disparity Map', disparity_normalized)
+    cv2.imwrite('disparity_map.jpg', disparity_normalized)
 
-    # Нормализуйте карту глубины
-    disparity = cv2.normalize(disparity, None, 0, 255, cv2.NORM_MINMAX)
-    disparity = np.uint8(disparity)
+def get_point_cloud(output_inverted, image):
+    width, height = image.size
 
-    #-------------------------------------------------------------------------------------
-    # # Создание детектора
-    # orb = cv2.ORB_create()
-    #
-    # # Нахождение ключевых точек и дескрипторов
-    # kp_left, des_left = orb.detectAndCompute(imgL, None)
-    # kp_right, des_right = orb.detectAndCompute(imgR, None)
-    # print(kp_left)
+    depth_image = (output_inverted * 255 / np.max(output_inverted)).astype('uint8')
+    image = np.array(image)
 
-    # Сопоставление дескрипторов
-    # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    # matches = bf.match(des_left, des_right)
+    # create rgbd image
+    depth_o3d = o3d.geometry.Image(depth_image)
+    image_o3d = o3d.geometry.Image(image)
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(image_o3d, depth_o3d,
+                                                                    convert_rgb_to_intensity=False)
 
-    # Сортировка совпадений
-    # matches = sorted(matches, key=lambda x: x.distance)
-    # print(matches)
+    # camera settings
+    camera_intrinsic = o3d.camera.PinholeCameraIntrinsic()
+    camera_intrinsic.set_intrinsics(width, height, 300, 300, width / 2, height / 2)
 
-    # points1 = np.array([kp_left[m.queryIdx].pt for m in matches])
-    # points2 = np.array([kp_right[m.trainIdx].pt for m in matches])
+    # create point cloud
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+    return pcd
 
-    #-----------------------------------------------------------------------------
-    # блок создания трехмерных точек
-    # Преобразуйте карту глубины в 3D-координаты
-    h, w = disparity.shape
-    Q = np.float32([[1, 0, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 0, 0, -focal_length],
-                    [0, 0, 1/baseline, 0]])
+def create_mesh(pcd):
+    cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=20.0)
+    pcd = pcd.select_by_index(ind)
 
-    points_3D = cv2.reprojectImageTo3D(disparity, Q)
+    # estimate normals
+    pcd.estimate_normals()
+    pcd.orient_normals_to_align_with_direction()
 
-    print(points_3D.ndim)
+    # surface reconstruction
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=10, n_threads=1)[0]
 
-    # if i > 1:
-    #     print(points_3D)
-    #     print('AND')
-    #     points_3D = shift_image(points_3D, dispart_late,  shift_amount=100)
-    #     print(points_3D)
-    #
-    # dispart_late = disparity
-
-    # Маска для действительных значений
-    mask = disparity > disparity.min()
-
-    # Извлеките действительные точки и цвета
-    valid_points = points_3D[mask]
-
-    #выводм колчество полученных точек
-    print("Count point valid_points: ", len(valid_points))
-    print('Min: '+ str(min(valid_points[2])))
-
-    # выводм колчество полученных точек
-    print("Count point valid_points: ", len(valid_points))
-
-    valid_colors = cv2.cvtColor(grayL, cv2.COLOR_GRAY2RGB)[mask]
-    if i > 1:
-        valid_points = find_new_point(valid_points, i)
-    # ----------------------------------------------------------------------------
-
-    # -------------------------------------------------------------------
-    # Преобразуйте в формат, подходящий для Open3D
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(valid_points)
-    pcd.colors = o3d.utility.Vector3dVector(valid_colors / 255.0)
-
-    # отсеиваем половину, поскольку комп может не выдержать большой нагрузки
-    # rezult_pcd = pcd.uniform_down_sample(every_k_points=2)
-    rezult_pcd = pcd
-
-    print("Count point rezult_pcd: ", len(rezult_pcd.points))
-
-    # ----------------------------------------------------------------------------
-    # совемещение облака точек
-    if i > 1:
-        # Объединение облаков
-        combined_pcd = glodal_cloud_point + rezult_pcd
-
-        # Удаление дубликатов
-        combined_pcd = combined_pcd.voxel_down_sample(voxel_size=0.01)
-        glodal_cloud_point = combined_pcd
-    else:
-        glodal_cloud_point = rezult_pcd
-
-# Сохраните облако точек в файл
-o3d.io.write_point_cloud('point_cloud.ply', glodal_cloud_point)
-
-print("Облако точек сохранено в 'point_cloud.ply'")
-# -------------------------------------------------------------------------
-
-# визуализация облака точек
-o3d.visualization.draw_geometries([glodal_cloud_point])
-# -------------------------------------------------------------------------
-
-
-# # Визуализация облака точек с помощью matplotlib
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# xyz_point = np.asarray(glodal_cloud_point.points)
-#
-# # Извлечение координат
-# x = xyz_point[:, 0]
-# y = xyz_point[:, 1]
-# z = xyz_point[:, 2]
-#
-# # Отображение облака точек
-# ax.scatter(x, y, z)
-#
-# # Настройка меток осей
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
-#
-# # Настройка пределов осей
-# ax.set_xlim([15000, -2000])
-# ax.set_ylim([1000, -15000])
-# ax.set_zlim([1000, -15000])
-#
-# # Показать график
-# plt.show()
-
-
-
-#-------------------------------------------------------------------------------
-# Фильтрация статистических выбросов
-cl, ind = glodal_cloud_point.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
-filtered_pcd = glodal_cloud_point.select_by_index(ind)
-# filtered_pcd = glodal_cloud_point
-print("Filter good")
-#------------------------------------------------------------------------------
-
-'''
-# Извлечение массива точек
-points = np.asarray(filtered_pcd.points)
-
-# print(points)
-'''
-
-
-# Вычисление нормалей
-filtered_pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-
-if filtered_pcd.has_normals():
-    print('Normals good')
-else:
-    print('Normals empty')
-    filtered_pcd.estimate_normals(serch_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-    print('Normals end')
-
-# Примените метод понижения размерности (например, ball pivoting)
-# mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
-#     glodal_cloud_point,
-#     o3d.utility.DoubleVector([0.4, 0.3, 0.4])  # Задайте радиусы для метода
-# )
-# mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-#     filtered_pcd, depth=11
-# )
-# filtered_pcd
-# surface reconstruction
-mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(filtered_pcd, depth=10, n_threads=1)[0]
-
-# rotate the mesh
-rotation = mesh.get_rotation_matrix_from_xyz((np.pi, 0, 0))
-# mesh.rotate(rotation, center=(0, 0, 0))
-
-if mesh.is_empty():
-    print('Mesh empty!')
-else:
-    # Визуализируйте полученный мэш
-    print('Mesh good!')
+    # rotate the mesh
+    rotation = mesh.get_rotation_matrix_from_xyz((np.pi, 0, 0))
+    mesh.rotate(rotation, center=(0, 0, 0))
     o3d.visualization.draw_geometries([mesh])
+
+    # save the mesh
+    # o3d.io.write_triangle_mesh(f'./mesh.obj', mesh)
+
+# # load and resize the input image
+# disparity = cv2.imread("disparity_map.jpg", cv2.IMREAD_UNCHANGED)
+#
+#
+# new_height = 480 if image.height > 480 else image.height
+# new_height -= (new_height % 32)
+# new_width = int(new_height * image.width / image.height)
+# diff = new_width % 32
+# new_width = new_width - diff if diff < 16 else new_width + 32 - diff
+# new_size = (new_width, new_height)
+# image = image.resize(new_size)
+#
+# # prepare image for the model
+# inputs = feature_extractor(images=image, return_tensors="pt")
+
+# Load the disparity map
+# load and resize the input image
+
+output_inverted, image = get_map()
+
+pcd = get_point_cloud(output_inverted, image)
+
+# plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=1000)
+# inlier_cloud = pcd.select_by_index(inliers)
+# outlier_cloud = pcd.select_by_index(inliers, invert=True)
+# inlier_cloud.paint_uniform_color([1, 0, 0])
+# outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
+#
+# o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+#--------------------------------------------------------------------------
+
+# from sklearn.cluster import DBSCAN
+#
+# # Загрузка облака точек
+# # pcd = o3d.io.read_point_cloud("path_to_your_point_cloud.ply")
+# points = np.asarray(pcd.points)
+#
+# # Кластеризация
+# dbscan = DBSCAN(eps=0.05, min_samples=10)
+# labels = dbscan.fit_predict(points)
+#
+# # Визуализация результатов
+# max_label = labels.max()
+# colors = plt.get_cmap("jet")(labels / max_label)  # Используем цветовую карту
+# pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])  # Убираем альфа-канал
+#
+# o3d.visualization.draw_geometries([pcd])
+#
+# # Сегментация плоскости с использованием RANSAC
+# plane_model, inliers = pcd.segment_plane(distance_threshold=2,
+#                                          ransac_n=5,
+#                                          num_iterations=1000)
+#
+# # Извлечение точек плоскости и остальных точек
+# inlier_cloud = pcd.select_by_index(inliers)
+# outlier_cloud = pcd.select_by_index(inliers, invert=True)
+#
+# # Визуализация результатов
+# inlier_cloud.paint_uniform_color([1, 0, 0])  # Красный для плоскости
+# outlier_cloud.paint_uniform_color([0, 1, 0])  # Зеленый для остальных точек
+#
+# o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+
+create_mesh(pcd)
+
